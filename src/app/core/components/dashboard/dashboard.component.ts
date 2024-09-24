2ad4f88c93b04c126813045d2b55b58124aacea8
sujitha-sekar <snowashii2002@gmail.com>
2024-09-24 17:06:26 +0530

    updation

--- src/app/core/components/dashboard/dashboard.component.ts
+++ src/app/core/components/dashboard/dashboard.component.ts
@@ -7,33 +7,13 @@ import { Component, HostListener, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  books = [
-    { id: 1, imageUrl: './assets/book-cover7.jpg', title: 'The Girl of Ink & Stars', description: 'Most Popular', author: 'Kiran Millwood Hargrave'},
-    { id: 2, imageUrl: './assets/book-cover9.jpg', title: 'Harry Potter', description: 'Fiction', author: 'J.K.Rowling'},
-    { id: 3, imageUrl: './assets/book-cover10.jpg', title: 'The Mirenda Prodject', description: 'Science & Research', author: 'Matthew Cesca'},
-    { id: 4, imageUrl: './assets/book-cover8.jpg', title: 'The Imperfection of Memory', description: 'Fiction', author: 'Angelina Aludo'},
-    { id: 5, imageUrl: './assets/book-cover1.jpg', title: 'The Little', description: 'Children', author: 'Jean Lumir'},
-    { id: 6, imageUrl: './assets/book-cover3.jpg', title: 'The Dream Arts', description: 'Art & Painting', author: 'Tom Maloney'},
-    { id: 7, imageUrl: './assets/book-cover2.jpg', title: 'Head Kid', description: 'Children', author: 'David Baddiel'},
-    { id: 8, imageUrl: './assets/book-cover6.jpg', title: 'The Journey to Moon', description: 'Science & Research', author: 'Max Bron'},
-  ]
+  ngOnInit() { }
 
-  @HostListener('window:scroll', ['$event'])
-  checkScroll() {
-    const navbar = document.getElementById('navbar');
-    
-    if (navbar) {
-      if(document.documentElement.scrollTop > 250) {
-        navbar.style.top = "0";
-      } else {
-        navbar.style.top = "-100px";
-      }
+  scrollToSection(value: string) {
+    const element = document.getElementById(value);
+    if (element) {
+      element.scrollIntoView({ behavior: 'smooth' });
     }
   }
-  
-  
- ngOnInit() {
-
-}
 
 }

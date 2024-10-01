ea19b3bba574de8600ca32b18cdac125fd42ef30
sujitha-sekar <snowashii2002@gmail.com>
2024-10-01 15:10:29 +0530

    updation

--- src/app/core/components/add-project/add-project.component.ts
+++ src/app/core/components/add-project/add-project.component.ts
@@ -7,4 +7,38 @@ import { Component } from '@angular/core';
 })
 export class AddProjectComponent {
 
+  demoMessage: string = '';
+
+  dragStart(event: DragEvent) {
+    if (event.target instanceof HTMLElement) {
+      event.dataTransfer?.setData("text", event.target.id);
+    }
+  }
+
+  dragging(event: DragEvent) {
+    this.demoMessage = "The p element is being dragged";
+  }
+
+  allowDrop(event: DragEvent) {
+    event.preventDefault();
+  }
+
+  drop(event: DragEvent) {
+    event.preventDefault();
+    
+    const data = event.dataTransfer?.getData("text");
+  
+    if (data) {  // Check if data is not undefined
+      const draggedElement = document.getElementById(data);
+  
+      if (draggedElement && event.target instanceof HTMLElement) {
+        event.target.appendChild(draggedElement);
+      }
+  
+      this.demoMessage = "The p element was dropped";
+    } else {
+      console.error('No data found in the drag event.');
+    }
+  }
+  
 }

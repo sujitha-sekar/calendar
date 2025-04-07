80b900918673e0875f8eba3d0dd5bb4e3fb2bc61
sujitha-sekar <snowashii2002@gmail.com>
2025-04-07 12:07:25 +0530

    adding express

---
+++ express/controllers/employee.controller.js
@@ -0,0 +1,11 @@
+const BlogList = require('../models/blog-list');
+require('../global_functions');
+
+const getAllBlogs = async function (req, res) {
+  let err;
+  [err, blogList] = await to(BlogList.findAll());
+  if (err) return ReE(res, err, 422);
+  return ReS(res, { blogList });
+}
+module.exports.getAllBlogs = getAllBlogs;
+
---
+++ express/models/blog-list.js
@@ -0,0 +1,41 @@
+'use strict';
+
+// Define a model for department table
+module.exports = (sequelize, DataTypes) => {
+  var Model = sequelize.define('bloglist', {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    blogId: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    blogTitle: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    blogDescription: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    created: {
+      type: DataTypes.DATE,
+      defaultValue: DataTypes.NOW,
+      allowNull: false
+    },
+    modified: {
+      type: DataTypes.DATE,
+      defaultValue: DataTypes.NOW,
+      allowNull: false
+    }
+  }, {
+    tableName: 'bloglist'
+  });
+  // Adding a class level method.
+  Model.associate = function (models) {
+    // this.employee = this.hasMany(models.employee);/
+  };
+  return Model;
+};
